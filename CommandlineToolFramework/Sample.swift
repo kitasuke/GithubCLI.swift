9715d1d05d33e411cf1998a72e18fd0e362f3dda
kitasuke <yusuke2759@gmail.com>
2015-07-19 16:44:22 +0900

    Doesn't work yet

--- CommandlineToolFramework/Sample.swift
+++ CommandlineToolFramework/Sample.swift
@@ -1,11 +1,13 @@
 //
-//  main.swift
+//  Sample.swift
 //  CommandlineTool
 //
 //  Created by Yusuke Kita on 7/19/15.
 //  Copyright (c) 2015 kitasuke. All rights reserved.
 //
 
-import Foundation
-
-println("Hello World")
+public struct Sample {
+    public init?(text: String) {
+        return nil
+    }
+}
